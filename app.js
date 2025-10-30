3d8352691de4eb9d1183ee86a525c2e88e8a74b8
khalilsediq <khalilullahsadique1334@gmail.com>
2025-10-30 10:26:13 +0500

    DOM_

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// const change = document.querySelector
+// ("#message");
+// change.innerHTML = "WELLCOME";
+// change.style.color = "blue";
+// change.style.fontSize = "24px";
+// change.style.fontWeight = "bold";
+
+
+// const result = document.querySelector("#result");
+// const input1 = document.querySelector("#myInput");
+
+// function showInput() {
+//     result.innerHTML = input1.value;
+//     result.style.color = "green";
+//     result.style.fontSize = "20px";
+//     result.style.fontWeight = "bold";
+//     input1.value = "";
+// }
